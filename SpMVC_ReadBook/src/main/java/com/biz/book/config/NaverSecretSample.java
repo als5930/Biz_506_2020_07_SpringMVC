8ca39de170100f72c32666b511d9064f9478f0e2
als5930 <als3478@naver.com>
2020-09-21 10:19:01 +0900

    ReadBook

---
+++ SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecretSample.java
@@ -0,0 +1,14 @@
+package com.biz.book.config;
+
+public class NaverSecretSample {
+
+	/*
+	 * 네이버 Open APL 등록을 수행한 후
+	 * Client ID 와 Client Secret를 복사햐여 등록한 후
+	 * 이 클래스를 NaverSevret로 변경하여 프로젝트 실행하시오
+	 * 
+	 */
+	public final static String NAVER_CLIENT_ID = "YOUR NAVER CLIENT ID";
+	public final static String NAVER_CLIENT_SECRET = "YOUR NAVER SECRET";
+	
+}
