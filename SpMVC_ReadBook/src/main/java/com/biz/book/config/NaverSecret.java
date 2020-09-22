2aa7124f4d0419910a6b228c64febd4921883501
als5930 <65649302+als5930@users.noreply.github.com>
2020-09-22 19:03:18 +0900

    Update NaverSecret.java

--- SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecret.java
+++ SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecret.java
@@ -2,7 +2,8 @@ package com.biz.book.config;
 
 public class NaverSecret {
 
-	
+	public final static String NAVER_CLIENT_ID = "YOUR NAVER CLIENT ID";
+	public final static String NAVER_CLIENT_SECRET = "YOUR NAVER SECRET";
 	
 	public final static String NAVER_BOOK_XML = "https://openapi.naver.com/v1/search/book.xml";
 	public final static String NAVER_BOOK_JSON = "https://openapi.naver.com/v1/search/book.json";
