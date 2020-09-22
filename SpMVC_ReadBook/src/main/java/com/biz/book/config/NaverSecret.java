addd41b19028fe72f2faafb4e8070502c1574e72
als5930 <65649302+als5930@users.noreply.github.com>
2020-09-22 19:01:03 +0900

    Add files via upload

---
+++ SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecret.java
@@ -0,0 +1,12 @@
+package com.biz.book.config;
+
+public class NaverSecret {
+
+	public final static String NAVER_CLIENT_ID = "ukOF5c54vzPP_rhHBMtr";
+	public final static String NAVER_CLIENT_SECRET = "hQqQTnbVu6";
+	
+	public final static String NAVER_BOOK_XML = "https://openapi.naver.com/v1/search/book.xml";
+	public final static String NAVER_BOOK_JSON = "https://openapi.naver.com/v1/search/book.json";
+	
+	
+}
